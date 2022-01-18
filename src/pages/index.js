857e5180ff4044b5f93e8c4dbf48e422d852f2bc
Njengak11 <njengak11@gmail.com>
2022-01-18 18:44:12 +0300

    added styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React from "react"
+import { Link } from 'gatsby'
 import Layout from "../components/Layout"
+import '../styles/home.css'
 
 export default function Home() {
   return (
     <Layout>
-    <section>
-    <div>
-      <h2>Design</h2>
-      <h3>Develop & Deploy</h3>
-      <p>UX designer & web developer based in Nairobi.</p>
-    </div>
-  </section>
-  </Layout>
+      <section className='header'>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Nairobi.</p>
+          <Link className="btn"  to="/projects">My Portfolio Projects</Link>
+        </div>
+      </section>
+    </Layout>
   )
 }
